ac62cb79be3bd226f529d3fa4a454c8602a403f7
Raman-120 <ramanchaulagain059@gmail.com>
2026-04-17 21:36:10 +0545

    improved logic flow and added not to create account feature

--- Projects/src/simpleBankingSystem.java
+++ Projects/src/simpleBankingSystem.java
@@ -12,32 +12,39 @@ public class simpleBankingSystem {
         String username = "raman";
         int pin= 455;
 
-
         System.out.print("Do you have an account in our bank?: ");
         response = scanner.nextLine();
 
-        if(response.equals("no")){
-            System.out.print("Would you like to create one: ");
-            response = scanner.nextLine();
-            if(response.equals("yes")){
-                account.basic();
+
+            if(response.equals("no")){
+                System.out.print("Would you like to create one: ");
+                response = scanner.nextLine();
+                if(response.equals("yes")){
+                    account.basic();
+                }
+                else {
+                    System.out.println("Thanks for visiting our site.");
+                    System.exit(0);
+                }
+            }
+            else{
+                account.alreadyExist();
+                if(username.equals(account.getUsername()) && pin == account.getPin()){
+                    activities.setBalance(balance);
+
+                }
+                else {
+                    System.out.println("Incorrect username or password.");
+                }
+
+            }
+
+            while(true){
                 activities.Menu();
                 activities.action();
             }
-        }
-        else{
-            account.alreadyExist();
-            if(username.equals(account.getUsername()) && pin == account.getPin()){
-                activities.setBalance(balance);
-                activities.Menu();
-                activities.action();
 
-            }
-            else {
-                System.out.println("Incorrect username or password.");
-            }
 
-        }
 
     }
 }
